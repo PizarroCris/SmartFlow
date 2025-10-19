fc0a460319668e64af078aa083969ed01d994dbd
Cristiano Pizarro <dev.cristianopizarro@gmail.com>
2025-10-19 15:20:46 +0100

    feat: Finalized user model logic, validations, and Devise setup (incl. :trackable).

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :trackable
+         
+  has_many :weekly_goals
+  has_many :tasks
+  
+  validates :name, presence: true
+  validates :email, uniqueness: { case_sensitive: false }
 end
