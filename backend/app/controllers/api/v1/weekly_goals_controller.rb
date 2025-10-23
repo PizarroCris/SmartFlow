d066059a73ceea5d755f8f83edad19c17d9b7d8b
PizarroCris <dev.cristianopizarro@gmail.com>
2025-10-23 09:28:43 +0100

    feat: Finalized WeeklyGoals CRUD using Jbuilder views for clean rendering and fully adhering to REST/Pundit/DRY best practices.

--- backend/app/controllers/api/v1/weekly_goals_controller.rb
+++ backend/app/controllers/api/v1/weekly_goals_controller.rb
@@ -1,51 +1,44 @@
-module Api
-  module V1
-    class WeeklyGoalsController < ApplicationController
-      before_action :authenticate_user!
+class Api::V1::WeeklyGoalsController < Api::V1::BaseController
+  before_action :set_weekly_goal, only: [:show, :update, :destroy]
 
-      def index
-        @weekly_goals = WeeklyGoal.where(user: current_user)
-        render json: @weekly_goals
-      end
+  def index
+    @weekly_goals = policy_scope(WeeklyGoal)
+  end
 
-      def show
-        @weekly_goal = current_user.weekly_goals.find(params[:id])
-        authorize @weekly_goal
-        render json: @weekly_goal
-      end
+  def show
+  end
 
-      def create
-        @weekly_goal = current_user.weekly_goals.build(weekly_goal_params)
-        authorize @weekly_goal
-        if @weekly_goal.save
-          render json: @weekly_goal, status: :created
-        else
-          render json: { errors: @weekly_goal.errors }, status: :unprocessable_entity
-        end
-      end
-
-      def update
-        @weekly_goal = current_user.weekly_goals.find(params[:id])
-        authorize @weekly_goal
-        if @weekly_goal.update(weekly_goal_params)
-          render json: @weekly_goal, status: :ok 
-        else
-          render json: { errors: @weekly_goal.errors }, status: :unprocessable_entity
-        end
-      end
-
-      def destroy
-        @weekly_goal = current_user.weekly_goals.find(params[:id])
-        authorize @weekly_goal
-        @weekly_goal.destroy
-        head :no_content
-      end
-
-      private
-      
-      def weekly_goal_params
-        params.require(:weekly_goal).permit(:title, :description, :target_date)
-      end
+  def create
+    @weekly_goal = current_user.weekly_goals.build(weekly_goal_params)
+    authorize @weekly_goal
+    if @weekly_goal.save
+      render :show, status: :created
+    else
+      render json: { errors: @weekly_goal.errors }, status: :unprocessable_entity
     end
+  end
+
+  def update
+    if @weekly_goal.update(weekly_goal_params)
+      render :show, status: :ok 
+    else
+      render json: { errors: @weekly_goal.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @weekly_goal.destroy
+    head :no_content
+  end
+
+  private
+
+  def set_weekly_goal
+    @weekly_goal = current_user.weekly_goals.find(params[:id])
+    authorize @weekly_goal
+  end
+  
+  def weekly_goal_params
+    params.require(:weekly_goal).permit(:title, :description, :target_date)
   end
 end
---
+++ backend/app/views/api/v1/weekly_goals/show.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! @weekly_goal, :id, :title, :description, :target_date
+json.user_id @weekly_goal.user_id 
